0f94d5bb333739195edccdbcd56c6d6587307dcb
tretek-prog <70658598+tretek-prog@users.noreply.github.com>
2021-04-27 21:46:20 +0300

    Update cheaker.py

--- cheaker.py
+++ cheaker.py
@@ -1,5 +1,15 @@
 import winreg
 
+# Функция проверки наличия sub_key System (по умолчанию его обычно нет).
+def system():
+    key_to_read = r'Software\Microsoft\Windows\CurrentVersion\Policies\System'
+    try:
+        reg = ConnectRegistry(None, HKEY_CURRENT_USER)
+        k = OpenKey(reg, key_to_read)
+        print("Есть")     # Действие если есть.
+    except:
+        print("Добавить") # Действие если нет (например добавить).
+
 # Функция проверки наличия ключа блокировки (невозможности открытия) диспетчера задач.
 def proverka_registry():
     value = [] # Массив, куда будут записываться все ключи sub_key System.
@@ -12,10 +22,10 @@ def proverka_registry():
         except:
             break
     print(value)
-    if (('DisableTaskMgr', 1, 4)) in value: # Проверка массива на ключ
-        print("OK")              # Действие, если ключ есть
+    if (('DisableTaskMgr', 1, 4)) in value: # Проверка массива на ключ.
+        print("OK")              # Действие, если ключ есть.
     else:
-        print("Что-нибудь")      # Действие, если ключа нет (например добавление оного)
+        print("Что-нибудь")      # Действие, если ключа нет (например добавление оного).
 
 # Функция проверки наличия ключа блокировки (невозможности открытия) встроенного редактора реестра.       
 def proverka_dispetcher():
@@ -29,7 +39,7 @@ def proverka_dispetcher():
         except:
             break
     print(value)
-    if (('DisableRegistryTools', 1, 4)) in value: # Проверка массива на ключ
-        print("OK")              # Действие, если ключ есть
+    if (('DisableRegistryTools', 1, 4)) in value: # Проверка массива на ключ.
+        print("OK")              # Действие, если ключ есть.
     else:
-        print("Что-нибудь")      # Действие, если ключа нет (например добавление оного)        
+        print("Что-нибудь")      # Действие, если ключа нет (например добавление оного) .       
