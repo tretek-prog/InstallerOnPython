bfd49dbba9cf98dc91c2a78104a2285ddfc04b7b
tretek-prog <70658598+tretek-prog@users.noreply.github.com>
2021-04-27 15:59:39 +0300

    Update cheaker.py

--- cheaker.py
+++ cheaker.py
@@ -2,7 +2,7 @@ import winreg
 
 value = [] # Массив, куда буду записываться все ключи
 j = 0
-startup = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\ZZZ")
+startup = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersions\\Run")
 while 1:
     try:
         value.append(winreg.EnumValue(startup, j)) # Запись ключей в массив
