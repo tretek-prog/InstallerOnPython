e69cb926e47edb052a218c4f3092ea849e5be78c
tretek-prog <70658598+tretek-prog@users.noreply.github.com>
2021-04-27 20:52:13 +0300

    Update cheaker.py

--- cheaker.py
+++ cheaker.py
@@ -1,12 +1,18 @@
 import winreg
 
-value = [] # Массив, куда буду записываться все ключи
-j = 0
-startup = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersions\\Run")
-while 1:
-    try:
-        value.append(winreg.EnumValue(startup, j)) # Запись ключей в массив
-        j += 1
-    except:
-        break
-print(value)
+# Функция наличия ключей блокировки встроенного редактора реестра и диспетчера задач.
+def proverkablock():
+    value = [] # Массив, куда будут записываться все ключи
+    j = 0
+    startup = reg.OpenKey(reg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Policies")
+    while 1:
+        try:
+            value.append(reg.EnumValue(startup, j)) # Запись ключей в массив
+            j += 1
+        except:
+            break
+    print(value)
+    if (('ff', '', 0)) in value: # Проверка массива на определенный ключ
+        print("OK")              # Действие, если ключ есть
+    else:
+        print("Что-нибудь")      # Действие, если ключа нет (например добавление оного)
