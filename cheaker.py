732eae97e90481b2dbd67d20ded0291bb6d685c1
tretek-prog <70658598+tretek-prog@users.noreply.github.com>
2021-04-27 21:25:13 +0300

    Update cheaker.py

--- cheaker.py
+++ cheaker.py
@@ -1,18 +1,18 @@
 import winreg
 
-# Функция наличия ключей блокировки встроенного редактора реестра и диспетчера задач.
-def proverkablock():
-    value = [] # Массив, куда будут записываться все ключи
+# Функция наличия ключа блокировки (невозможности открытия) диспетчера задач.
+def proverka_registry():
+    value = [] # Массив, куда будут записываться все ключи sub_key System.
     j = 0
     startup = reg.OpenKey(reg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System")
     while 1:
         try:
-            value.append(reg.EnumValue(startup, j)) # Запись ключей в массив
+            value.append(reg.EnumValue(startup, j)) # Запись всех ключей sub_key System в массив.
             j += 1
         except:
             break
     print(value)
-    if (('DisableTaskMgr', 1, 4)) in value: # Проверка массива на определенный ключ
+    if (('DisableTaskMgr', 1, 4)) in value: # Проверка массива на ключ
         print("OK")              # Действие, если ключ есть
     else:
         print("Что-нибудь")      # Действие, если ключа нет (например добавление оного)
